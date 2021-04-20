e29518707e6f933c66853d0a3304cc3893e1fe72
romero-c-raul <raulromerocano@aggienetwork.com>
2021-04-20 20:32:23 -0230

    Implemented functionality to create, complete, and delete todos

--- database_persistence.rb
+++ database_persistence.rb
@@ -44,35 +44,28 @@ class DatabasePersistence
   end
 
   def update_list_name(id, new_name)
-    # list = find_list(id)
-    # list[:name] = new_name
     sql = "UPDATE lists SET name = $1 WHERE id = $2"
     query(sql, new_name, id)
   end
 
   def create_new_todo(list_id, todo_name)
-    # list = find_list(list_id)
-    # id = next_element_id(list[:todos])
-    # list[:todos] << { id: id, name: todo_name, completed: false }
+    sql = "INSERT INTO todos (list_id, name) VALUES ($1, $2)"
+    query(sql, list_id, todo_name)
   end
 
   def delete_todo_from_list(list_id, todo_id)
-    # list = find_list(list_id)
-    # list[:todos].reject! { |todo| todo[:id] == todo_id }
+    sql = "DELETE FROM todos WHERE list_id = $1 AND id = $2"
+    query(sql, list_id, todo_id)
   end
 
   def update_todo_status(list_id, todo_id, new_status)
-    # list = find_list(list_id)
-    # todo = list[:todos].find { |t| t[:id] == todo_id }
-    # todo[:completed] = new_status
+    sql = "UPDATE todos SET completed = $3 WHERE list_id = $1 AND id = $2"
+    query(sql, list_id, todo_id, new_status)
   end
 
   def mark_all_todos_as_completed(list_id)
-    # list = find_list(list_id)
-
-    # list[:todos].each do |todo|
-    #   todo[:completed] = true
-    # end
+    sql = "UPDATE todos SET completed = true WHERE list_id = $1"
+    query(sql, list_id)
   end
 
   private
